180c194388f0bcb584be89653fbfc13ca75baf58
jhrick <hj.rickcp@gmail.com>
2023-10-02 12:58:38 -0300

    create client model

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,4 @@
 package org.example;
-
-import at.favre.lib.crypto.bcrypt.BCrypt;
-
 public class Main {
     public static void main(String[] args) {
 
---
+++ src/main/java/org/example/client/Client.java
@@ -0,0 +1,44 @@
+package org.example.client;
+
+import java.util.UUID;
+
+public class Client {
+    private final String id = UUID.randomUUID().toString();
+    private String name;
+    private int age;
+    private String password;
+
+    public Client(String name, int age, String password) {
+        this.name = name;
+        this.age = age;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
